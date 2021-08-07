848bbe966a7abf68a3e06a0c700bff8fbaa0809f
Juliano Freitas de Souza <rj.jqru@gmail.com>
2021-08-07 12:37:50 -0300

    Modificações

--- quinzena2/blackjack/desafio.js
+++ quinzena2/blackjack/desafio.js
@@ -23,60 +23,57 @@
 
 
    const AllGame = (userCards, computerCards) => {
-   let userScore = userCards[0].valor + userCards[1].valor 
-   let computerScore = computerCards[0].valor + computerCards[1].valor
-   let guardaValores = []
-   let guardaValoresComputer = []
-   let newCard
-   let newCardComputer
-   guardaValoresComputer.push(computerCards[0].texto, computerCards[1].texto)
-   guardaValores.push(userCards[0].texto, userCards[1].texto)
-   console.log(`Suas cartas são ${userCards[0].texto} ${userCards[1].texto} - A carta revelada do computador é ${computerCards[0].texto} \n\nDeseja comprar mais uma carta?`)
-   while (userConfirm = confirm('Deseja comprar mais uma carta?') === true && userScore < 21) {
-      if (userConfirm === true) {
-         newCard = comprarCarta()
-         guardaValores.push(newCard.texto)
-         userScore += newCard.valor
-         let formattedTextUser = guardaValores.toString()
-         console.log(`Suas cartas são ${formattedTextUser.replaceAll(',',' ')} - A carta revelada do computador é ${computerCards[0].texto} \n\n`)
-      } else if (userConfirm !== true) {
-         let computerNewScore = computerCards[0].valor + computerCards[1].valor
-         console.log(`Suas cartas são ${formattedTextUser.replaceAll(',',' ')} - Sua pontuação é ${userScore}\n\nAs cartas do computador são ${computerCards[0].texto} ${computerCards[1].texto} - A pontuação do computador é ${computerNewScore}`)
+      let userScore = userCards[0].valor + userCards[1].valor
+      let computerScore = computerCards[0].valor + computerCards[1].valor
+      let guardaValores = []
+      let guardaValoresComputer = []
+      let newCard
+      let newCardComputer
+      guardaValoresComputer.push(computerCards[0].texto, computerCards[1].texto)
+      guardaValores.push(userCards[0].texto, userCards[1].texto)
+      console.log(`Suas cartas são ${userCards[0].texto} ${userCards[1].texto} - A carta revelada do computador é ${computerCards[0].texto} \n\nDeseja comprar mais uma carta?`)
+      while (userConfirm = confirm('Deseja comprar mais uma carta?') === true && userScore < 21) {
+         if (userConfirm) {
+            newCard = comprarCarta()
+            guardaValores.push(newCard.texto)
+            userScore += newCard.valor
+            let formattedTextUser = guardaValores.toString()
+            console.log(`Suas cartas são ${formattedTextUser.replaceAll(',',' ')} - A carta revelada do computador é ${computerCards[0].texto} \n\n`)
+         } else {
+            let computerNewScore = computerCards[0].valor + computerCards[1].valor
+            console.log(`Suas cartas são ${formattedTextUser.replaceAll(',',' ')} - Sua pontuação é ${userScore}\n\nAs cartas do computador são ${computerCards[0].texto} ${computerCards[1].texto} - A pontuação do computador é ${computerNewScore}`)
+         }
+      }
+
+      while (computerScore < 21) {
+         newCardComputer = comprarCarta()
+         guardaValoresComputer.push(newCardComputer.texto)
+         computerScore += newCardComputer.valor
+         formattedTextUser = guardaValores.toString()
+         let formattedTextComputer = guardaValoresComputer.toString()
+         console.log(`Suas cartas são ${formattedTextUser.replaceAll(',',' ')} - Sua pontuação é ${userScore}\n\nAs cartas do computador são ${formattedTextComputer.replaceAll(',',' ')} - A pontuação do computador é ${computerScore}`)
+      }
+
+      if (userScore <= 21 && computerScore > 21) {
+         console.log('O usuário ganhou!')
+      } else if (computerScore <= 21 && userScore > 21) {
+         console.log('O computador ganhou!')
+      } else if (userScore > 21 && computerScore > 21) {
+         console.log('Não houve ganhadores!')
+      } else if (userScore > computerScore) {
+         console.log('O usuário ganhou!')
+      } else if (userScore < computerScore) {
+         console.log('O computador ganhou!')
+      } else if (userScore === computerScore) {
+         console.log('Empate!')
       }
    }
-   
-   while (computerScore < 21) {
-      newCardComputer =  comprarCarta()
-      guardaValoresComputer.push(newCardComputer.texto)
-      computerScore +=  newCardComputer.valor
-      formattedTextUser = guardaValores.toString()
-      let formattedTextComputer = guardaValoresComputer.toString()
-      console.log(`Suas cartas são ${formattedTextUser.replaceAll(',',' ')} - Sua pontuação é ${userScore}\n\nAs cartas do computador são ${formattedTextComputer.replaceAll(',',' ')} - A pontuação do computador é ${computerScore}`)
+
+   console.log('Bem vindo ao jogo de Blackjack!')
+   if (confirm('Quer iniciar uma nova rodada?')) {
+      userCardsDraw()
+      computerCardsDraw()
+      AllGame(userCards, computerCards)
+   } else {
+      console.log('O jogo acabou!')
    }
-
-   if (userScore <= 21 && computerScore > 21) {
-      console.log('O usuário ganhou!')
-   } else if (computerScore <= 21 && userScore > 21) {
-      console.log('O computador ganhou!')
-   } else if (userScore > 21 && computerScore > 21) {
-      console.log('Não houve ganhadores!')
-   } else if (userScore > computerScore) {
-      console.log('O usuário ganhou!')
-   } else if (userScore < computerScore) {
-      console.log('O computador ganhou!')
-   } else if (userScore === computerScore) {
-      console.log('Empate!')
-   }
-}
-
-console.log('Bem vindo ao jogo de Blackjack!')
-if (confirm('Quer iniciar uma nova rodada?')) {
-   userCardsDraw()
-   computerCardsDraw()
-   AllGame(userCards, computerCards)
-}
-   else {
-   console.log('O jogo acabou!')
-}
-
-
--- quinzena2/blackjack/nossoJogo.js
+++ quinzena2/blackjack/nossoJogo.js
@@ -1,37 +1,37 @@
    const userGame = (userCard, userCard2) => {
-   const userScore = userCard.valor + userCard2.valor 
-   console.log(`Usuário - cartas: ${userCard.texto}, ${userCard2.texto} - pontuação ${userScore} `)
-   return userScore
-   
-}
+      const userScore = userCard.valor + userCard2.valor
+      console.log(`Usuário - cartas: ${userCard.texto}, ${userCard2.texto} - pontuação ${userScore} `)
+      return userScore
+
+   }
 
    const computerGame = (computerCard, computerCard2) => {
-   const computerScore = computerCard.valor + computerCard2.valor 
-   console.log(`Computador - cartas: ${computerCard.texto}, ${computerCard2.texto} - pontuação ${computerScore} `)
-   return computerScore
-}
+      const computerScore = computerCard.valor + computerCard2.valor
+      console.log(`Computador - cartas: ${computerCard.texto}, ${computerCard2.texto} - pontuação ${computerScore} `)
+      return computerScore
+   }
 
    const comparingWinner = (userScore, computerScore) => {
-   if (userScore > computerScore) {
-      console.log('O usuário ganhou!')
-   } else if (userScore < computerScore) {
-      console.log('O computador ganhou!')
-   } else {
-      console.log('Empate!')
+      if (userScore > computerScore) {
+         console.log('O usuário ganhou!')
+      } else if (userScore < computerScore) {
+         console.log('O computador ganhou!')
+      } else {
+         console.log('Empate!')
+      }
    }
-}
 
-console.log('Bem vindo ao jogo de Blackjack!')
-if (confirm('Quer iniciar uma nova rodada?')) {
-   console.log('O jogo vai começar!')
-   const userCard = comprarCarta()
-   const userCard2 = comprarCarta()
-   const computerCard = comprarCarta()
-   const computerCard2 = comprarCarta()
-   const userScore = userGame(userCard, userCard2)
-   const computerScore = computerGame(computerCard, computerCard2)
-   comparingWinner(userScore, computerScore)
-   
-}  else {
-   console.log('O jogo acabou')
-}
+   console.log('Bem vindo ao jogo de Blackjack!')
+   if (confirm('Quer iniciar uma nova rodada?')) {
+      console.log('O jogo vai começar!')
+      const userCard = comprarCarta()
+      const userCard2 = comprarCarta()
+      const computerCard = comprarCarta()
+      const computerCard2 = comprarCarta()
+      const userScore = userGame(userCard, userCard2)
+      const computerScore = computerGame(computerCard, computerCard2)
+      comparingWinner(userScore, computerScore)
+
+   } else {
+      console.log('O jogo acabou')
+   }
